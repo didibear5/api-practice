e091514af0058f699c6434e8435398a334b0e24a
邱佳禾 <s400499@sinyi.com.tw>
2019-01-29 11:27:00 +0800

    move mongodb to db.js

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 // 引入 module
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+
 // 產生一個 express instance - app
 const app = express()
 app.use(bodyParser.json())
 
-
-//Set up default mongoose connection
-const mongoDB = 'mongodb://localhost/testdb';
-mongoose.connect(mongoDB); 
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
-const db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+require('./db')
 require('./routes/post')(app)
 require('./routes/comment')(app)
 
